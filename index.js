bfea9455e3c004b5a035919f889c788c9adf9c28
NEELESH B SHETTY <neeleshshetty1236@gmail.com>
2023-08-26 15:31:45 +0530

    Create routes folder

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port = 8000;
 
 const app = express();
 
+//Express Router
+app.use("/", require("./routes/index"));
+
 app.listen(port, function (err) {
   if (err) {
     console.log("Error in conecting the server");
